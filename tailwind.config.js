b80e30602364cc323e2f0f467d3494278e4cd71a
francisudeji <francis.udejiofficial@gmail.com>
2019-09-19 07:30:57 +0100

    Cofigs for tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {},
+    container: {
+      center: true
+    },
+    screens: {
+      sm: '576px',
+      md: '768px'
+    }
+  },
+  variants: {},
+  plugins: []
+}
