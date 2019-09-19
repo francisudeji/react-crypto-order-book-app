08a1525636fb87b7140b03f14f6c6c22c6cebcb1
francisudeji <francis.udejiofficial@gmail.com>
2019-09-19 11:32:39 +0100

    Loading spinner component and css

---
+++ src/components/spinner/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './spinner.css'
+
+function Spinner() {
+  return (
+    <div className='spinner'>
+      <div className='spinner-inner'></div>
+    </div>
+  )
+}
+
+export default Spinner
