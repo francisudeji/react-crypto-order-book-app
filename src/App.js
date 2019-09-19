b583717f64002159231b8b1136dc346fa179a56d
francisudeji <francis.udejiofficial@gmail.com>
2019-09-19 07:38:34 +0100

    Houesekeeping and refactor

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import Dropdown from './components/dropdown'
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false)
+  const [tradingPairs, setTradingPairs] = useState([])
+
+  useEffect(() => {
+    const url = 'https://www.bitstamp.net/api/v2/trading-pairs-info/'
+    const getTradingPairs = async () => {
+      const response = await fetch(url)
+      const data = await response.text()
+      setTradingPairs(data)
+      console.log(data)
+    }
+    getTradingPairs()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <div className='container'>
+        <button
+          className='bg-white text-gray-500 font-semibold uppercase text-lg w-full px-6 py-3 border border-gray-200 flex justify-between items-center mt-10 outline-none hover:border-gray-400 focus:border-gray-400'
+          onClick={() => setIsOpen(!isOpen)}
         >
-          Learn React
-        </a>
-      </header>
+          <span>Select Currency Pair</span>
+          {isOpen ? <span>&uarr;</span> : <span>&darr;</span>}
+        </button>
+        <div>
+          {isOpen && tradingPairs.length > 0 && (
+            <Dropdown items={tradingPairs} />
+          )}
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
