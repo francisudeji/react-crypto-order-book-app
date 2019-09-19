e48332d9894121d83c86b474e9fe191f24ca7146
francisudeji <francis.udejiofficial@gmail.com>
2019-09-19 11:34:15 +0100

    Refactor and react router setup

--- src/App.js
+++ src/App.js
@@ -1,36 +1,17 @@
-import React, { useState, useEffect } from 'react'
-import Dropdown from './components/dropdown'
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './components/home'
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false)
-  const [tradingPairs, setTradingPairs] = useState([])
-
-  useEffect(() => {
-    const url = 'https://www.bitstamp.net/api/v2/trading-pairs-info/'
-    const getTradingPairs = async () => {
-      const response = await fetch(url)
-      const data = await response.text()
-      setTradingPairs(data)
-      console.log(data)
-    }
-    getTradingPairs()
-  }, [])
-
   return (
     <div className='App'>
-      <div className='container'>
-        <button
-          className='bg-white text-gray-500 font-semibold uppercase text-lg w-full px-6 py-3 border border-gray-200 flex justify-between items-center mt-10 outline-none hover:border-gray-400 focus:border-gray-400'
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          <span>Select Currency Pair</span>
-          {isOpen ? <span>&uarr;</span> : <span>&darr;</span>}
-        </button>
-        <div>
-          {isOpen && tradingPairs.length > 0 && (
-            <Dropdown items={tradingPairs} />
-          )}
-        </div>
+      <div className='container px-6'>
+        <Router>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            {/* <Route path='/:currency_pair' component={OrderBook} /> */}
+          </Switch>
+        </Router>
       </div>
     </div>
   )
