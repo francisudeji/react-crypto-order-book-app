31e2cf3be64dfbb7fbc4a0d5f67207409ec30658
francisudeji <francis.udejiofficial@gmail.com>
2019-09-20 12:24:48 +0100

    Wtapp app in provider

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './components/home'
+import OrderBook from './components/order-book'
+import { Provider } from './hooks/app-context'
 
 function App() {
   return (
     <div className='App'>
       <div className='container px-6'>
-        <Router>
-          <Switch>
-            <Route exact path='/' component={Home} />
-            {/* <Route path='/:currency_pair' component={OrderBook} /> */}
-          </Switch>
-        </Router>
+        <Provider>
+          <Router>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/:currency_pair' component={OrderBook} />
+            </Switch>
+          </Router>
+        </Provider>
       </div>
     </div>
   )
